1350ee3a960c8443fae6d2bd6104cf261b1d9f97
hhsNel <hihijsonel@gmail.com>
2025-05-09 12:12:34 +0200

    ANSI color codes

--- color.h
+++ color.h
@@ -1,10 +1,10 @@
-#define BG_RGB(R,G,B) ""
-#define FG_RGB(R,G,B) ""
-#define BG_256(C) ""
-#define FG_256(C) ""
-#define BG_USR(C) ""
-#define FG_USR(C) ""
+#define BG_RGB(R,G,B) "\033[48;2;" #R ";" #G ";" #B "m"
+#define FG_RGB(R,G,B) "\033[38;2;" #R ";" #G ";" #B "m"
+#define BG_256(C) "\033[48;5;" #C "m"
+#define FG_256(C) "\033[38;5;" #C "m"
+#define BG_USR(C) "\033[" #C "m"
+#define FG_USR(C) "\033[0" #C "m"
 
-#define TXT_BOLD ""
-#define TXT_BLINKING ""
+#define TXT_BOLD "\033[22m"
+#define TXT_BLINKING "\033[25m"
 
