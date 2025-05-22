e4734f0fa67b2b6c0ed3b72a36778fcede53bd28
hhsNel <hihijsonel@gmail.com>
2025-05-22 17:46:33 +0200

    color fixes

--- color.h
+++ color.h
@@ -5,11 +5,12 @@
 #define FG_RGB(R,G,B) "\033[38;2;" #R ";" #G ";" #B "m"
 #define BG_256(C) "\033[48;5;" #C "m"
 #define FG_256(C) "\033[38;5;" #C "m"
-#define BG_USR(C) "\033[" #C "m"
-#define FG_USR(C) "\033[0" #C "m"
+#define BG_USR(C) "\033[3" #C "m"
+#define FG_USR(C) "\033[4" #C "m"
 
-#define TXT_BOLD "\033[22m"
-#define TXT_BLINKING "\033[25m"
+#define TXT_BOLD "\033[1m"
+#define TXT_BLINKING "\033[5m"
+#define TXT_RESET "\033[0m"
 
 #endif
 
